511ff92285ff74e64ff56c2b1a586e888cffb378
Evan Thomas <evan.thomas@epfl.ch>
2023-09-12 17:05:24 +0200

    Remove other versions

--- WRF4.4_Install.bash
+++ WRF4.4_Install.bash
@@ -6,21 +6,6 @@
 #########################################################
 WRFversion="4.4"
 type="ARW"
-if [ -n "$1" ]; then
-    if [ "$1" = "-chem" ]; then
-        type="Chem"
-    elif [ "$1" = "-arw" ]; then
-        type="ARW"
-    elif [ "$1" = "-hydro" ]; then
-        type="Hydro"
-    else
-        echo "Unrecognized option, please run as"
-        echo "For WRF-ARW \"bash WRF${WRFversion}_Install.bash -arw\""
-        echo "For WRF-Chem \"bash WRF${WRFversion}_Install.bash -chem\""
-	echo "For WRF-Hydro \"bash WRF${WRFversion}_Install.bash -hydro\""
-        exit
-    fi
-fi
 echo "Welcome! This Script will install the WRF${WRFversion}-${type}"
 echo "Installation may take several hours and it takes 52 GB storage. Be sure that you have enough time and storage."
 #########################################################
@@ -42,18 +27,11 @@ else
 #Tested on Ubuntu 20.04
 exit
 fi
-local_language=$(locale | grep LANG | grep tr_TR)
-if [ -n "$local_language" ]; then
- echo "Merhaba, WRF modelinin kodundaki hatadan dolayı, WRF kurulumu işletim sistemi dili Türkçe olduğunda, Türkçedeki i ve ı harflerinin farklı olması sebebiyle hata vermektedir. Lütfen işletim sisteminizin dilini başka bir dile çevirip yeniden çalıştırınız. Kurulum bittikten sonra işletim sistemi dilini tekrar Türkçe'ye çevirebilirsiniz."
- exit
-fi
+
 #########################################################
 #   Installing neccesary packages                       #
 #########################################################
 
-if [ "$type" = "Chem" ]; then
- extra_packages="flex-old bison"
-fi
 echo "Please enter your sudo password, so necessary packages can be installed."
 sudo apt-get update
 mpich_repoversion=$(apt-cache policy mpich | grep Candidate | cut -d ':' -f 2 | cut -d '-' -f 1 | cut -c2)
@@ -101,12 +79,7 @@ export HDF5=/usr/lib/x86_64-linux-gnu/hdf5/serial
 export LDFLAGS="-L/usr/lib/x86_64-linux-gnu/hdf5/serial/ -L/usr/lib"
 export CPPFLAGS="-I/usr/include/hdf5/serial/ -I/usr/include"
 export LD_LIBRARY_PATH=/usr/lib
-if [ "$type" = "Chem" ]; then
-[[ -z $(grep "export FLEX_LIB_DIR=/usr/lib" ~/.bashrc) ]] && echo "export FLEX_LIB_DIR=/usr/lib" >> ~/.bashrc
-[[ -z $(grep "export YACC='yacc -d'" ~/.bashrc) ]] && echo "export YACC='yacc -d'" >> ~/.bashrc
-export FLEX_LIB_DIR=/usr/lib
-export YACC='yacc -d'
-fi
+
 ##########################################
 #	Jasper Installation		#
 #########################################
@@ -132,21 +105,7 @@ cd ..
 wget https://github.com/wrf-model/WRF/releases/download/v${WRFversion}/v${WRFversion}.tar.gz
 mv v${WRFversion}.tar.gz WRFV${WRFversion}.tar.gz
 tar -zxvf WRFV${WRFversion}.tar.gz
-if [ "$type" = "Hydro" ]; then
-export WRF_HYDRO=1
-[ -f "v5.2.0.tar.gz" ] && mv v5.2.0.tar.gz v5.2.0.tar.gz-old
-wget https://github.com/NCAR/wrf_hydro_nwm_public/archive/refs/tags/v5.2.0.tar.gz
-tar -zxvf v5.2.0.tar.gz
-/bin/cp -rf wrf_hydro_nwm_public-5.2.0/trunk/NDHMS/* WRFV${WRFversion}/hydro/
-rm v5.2.0.tar.gz
-rm -r wrf_hydro_nwm_public-5.2.0
-fi
 cd WRFV${WRFversion}
-if [ "$type" = "Chem" ]; then
-export WRF_CHEM=1
-export WRF_KPP=1
-sed -i '232s/.*/module_mosaic_addemiss.o: module_data_mosaic_asect.o module_data_sorgam.o module_gocart_dust.o/' chem/depend.chem
-fi
 sed -i 's#$NETCDF/lib#$NETCDF/lib/x86_64-linux-gnu#g' configure
 cd arch
 cp Config.pl Config.pl_backup
@@ -195,86 +154,6 @@ cd arch
 cp Config.pl_backup Config.pl
 cd ..
 cd ..
-#########################################
-#	Opening Geog Data Files 	#
-#########################################
-if [ -d "WPS_GEOG" ]; then
-  echo "WRF and WPS are installed successfully"
-  echo "Directory WPS_GEOG is already exists."
-  echo "Do you want WPS_GEOG files to be redownloaded and reexracted?"
-  echo "please type yes or no"
-  read GEOG_validation
-  if [ ${GEOG_validation} = "yes" ]; then
-    wget http://www2.mmm.ucar.edu/wrf/src/wps_files/geog_high_res_mandatory.tar.gz
-    tar -zxvf geog_high_res_mandatory.tar.gz
-  else
-    echo "You can download it later from http://www2.mmm.ucar.edu/wrf/src/wps_files/geog_high_res_mandatory.tar.gz and extract it"
-   fi
-else
-wget http://www2.mmm.ucar.edu/wrf/src/wps_files/geog_high_res_mandatory.tar.gz
-tar -zxvf geog_high_res_mandatory.tar.gz
-fi
-if [ "$type" = "Chem" ]; then
- cd WPS_GEOG
- Chem_Geog="modis_landuse_21class_30s soiltype_top_2m soiltype_bot_2m albedo_ncep maxsnowalb erod clayfrac_5m sandfrac_5m"
- for i in ${Chem_Geog}; do
-  if [ ! -d $i ]; then
-   echo $i
-   wget https://www2.mmm.ucar.edu/wrf/src/wps_files/${i}.tar.bz2 
-   tar -xvf ${i}.tar.bz2
-   rm ${i}.tar.bz2
-  fi
- done
- cd ..
-fi
-#######################################
-#       PREP_CHEM_SRC                 #
-#######################################
-if [ "$type" = "Chem" ]; then
- echo "Do you want the PREP-CHEM-SRC program to be installed? PREP-CHEM-SRC is a widely used emission preparation program for WRF-Chem."
- echo "please type yes or no"
- read prep_chem_validation
-  if [ ${prep_chem_validation} = "yes" ]; then
-  echo "firstly starting to compile convert_emiss.exe. convert_emiss.exe is needed for convert emissions which are created from PREP-CHEM-SRC." 
-  cd WRF-${WRFversion}-${type}
-  logsave convert_emi.log ./compile emi_conv
-  cd ..
-  echo "Compilation of convert_emiss.exe is finished, now PREP-CHEM-SRC download and compilation has started."
-  [ -d "PREP-CHEM-SRC-1.5" ] && mv PREP-CHEM-SRC-1.5 PREP-CHEM-SRC-1.5-old
-  [ -f "prep_chem_sources_v1.5_24aug2015.tar.gz" ] && mv prep_chem_sources_v1.5_24aug2015.tar.gz prep_chem_sources_v1.5_24aug2015.tar.gz-old
-  wget ftp://aftp.fsl.noaa.gov/divisions/taq/global_emissions/prep_chem_sources_v1.5_24aug2015.tar.gz
-  tar -zxvf prep_chem_sources_v1.5_24aug2015.tar.gz
-  cd PREP-CHEM-SRC-1.5/bin/build
-  sed -i "s#NETCDF=.*#NETCDF=/usr#" include.mk.gfortran.wrf
-  sed -i 's#-L$(NETCDF)/lib#-L/usr/lib/x86_64-linux-gnu#' include.mk.gfortran.wrf
-  sed -i "s#HDF5=.*#HDF5=/usr/lib/x86_64-linux-gnu/hdf5/serial#" include.mk.gfortran.wrf
-  sed -i "s#HDF5_INC=.*#HDF5_INC=-I/usr/include/hdf5/serial#" include.mk.gfortran.wrf
-  sed -i 's#-L$(HDF5)/lib#-L/usr/lib/x86_64-linux-gnu/hdf5/serial#' include.mk.gfortran.wrf
-  gfortversion=$(gfortran -dumpversion)
-  if [ "$gfortversion" -ge 10 ]; then
-  sed -i 's#F_OPTS=.*#F_OPTS=  -Xpreprocessor -D$(CHEM) -O2 -fconvert=big-endian -frecord-marker=4 -fallow-argument-mismatch#' include.mk.gfortran.wrf
-  fi
-  sed -i "s#-L/scratchin/grupos/catt-brams/shared/libs/gfortran/zlib-1.2.8/lib#-L/usr/lib#" include.mk.gfortran.wrf
-  sed -i "842s#.*#    'ENERGY     ',\&#" ../../src/edgar_emissions.f90
-  sed -i "843s#.*#    'INDUSTRY   ',\&#" ../../src/edgar_emissions.f90
-  sed -i "845s#.*#    'TRANSPORT  '/)#" ../../src/edgar_emissions.f90
-  make OPT=gfortran.wrf CHEM=RADM_WRF_FIM
-  cd ..
-  mkdir datain
-  cd datain
-  wget ftp://aftp.fsl.noaa.gov/divisions/taq/global_emissions/global_emissions_v3_24aug2015.tar.gz
-  tar -zxvf global_emissions_v3_24aug2015.tar.gz
-  mv Global_emissions_v3/* .
-  rm -r Global_emissions_v3
-  mv Emission_data/ EMISSION_DATA
-  mv surface_data/ SURFACE_DATA
-  cd ../../..
-  echo "PREP-CHEM-SRC compilation has finished."
-  fi
-fi
-##########################################################
-#	End						#
-##########################################################
 echo "Installation has completed"
 exec bash
 exit
